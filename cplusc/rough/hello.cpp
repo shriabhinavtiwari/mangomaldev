814470a5cc60665511ca8d4963a6a0b5758d61e3
shriabhinavtiwari <shriabhinavtiwari@gmail.com>
2024-04-18 01:12:28 +0530

    day_0: initial commit

---
+++ cplusc/rough/hello.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    vector<int> vtr(100,0);
+    for(auto it: vtr){
+        cout<<it<<endl;
+    }
+    return 0;
+}
---
+++ pythonprograms/main.py
@@ -0,0 +1,6 @@
+# python3
+
+import random
+
+def random_number(n:int)->int:
+	return random.random.number(k=15)
