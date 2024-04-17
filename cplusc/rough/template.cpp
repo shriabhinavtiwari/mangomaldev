3f7ed615bbae279b42ad1ce33e22016c4fd5b1e7
shriabhinavtiwari <shriabhinavtiwari@gmail.com>
2024-04-18 01:56:56 +0530

    coding template ready to be used

---
+++ cplusc/rough/template.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+#define ld long double 
+#define ll long long 
+#define F first
+#define S second
+#define EB emplace_back
+#define MP make_pair
+
+#define REP(i,a,n) for(int i=a;i<n;i++)
+#define COM(a,b) abs(a-b)<1e-9
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // solution
+    // cin and cout for input and output respectively
+    return 0;
+}
