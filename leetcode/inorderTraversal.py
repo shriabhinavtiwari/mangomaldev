e8223212929e945fd69fb4f952a3dbd52b7f1882
Abhinav Tiwari <shriabhinavtiwari@Mac.lan>
2026-02-13 09:42:40 +0530

    feat: marching towards a perfect victory

---
+++ leetcode/inorderTraversal.py
@@ -0,0 +1,18 @@
+from typing import Optional, List
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if not root:
+            return result
+        if root.left:
+            result.extend(self.inorderTraversal(root.left))
+        result.extend([root.val])
+        if root.right:
+            result.extend(self.inorderTraversal(root.right))
+        return result
